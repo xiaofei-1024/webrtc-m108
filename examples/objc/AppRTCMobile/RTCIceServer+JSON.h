f8371c5262a8c6b17275b10644a3dadd21b41851
xiaofei-1024 <allenxiaofei@163.com>
2024-09-01 23:30:10 +0800

    m108 h265

---
+++ examples/objc/AppRTCMobile/RTCIceServer+JSON.h
@@ -0,0 +1,22 @@
+/*
+ *  Copyright 2014 The WebRTC Project Authors. All rights reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#if defined(BUILD_WITHOUT_NINJA)
+@import WebRTC;
+#else
+#import "sdk/objc/api/peerconnection/RTCIceServer.h"
+#endif
+
+@interface RTC_OBJC_TYPE (RTCIceServer)
+(JSON)
+
+    + (RTC_OBJC_TYPE(RTCIceServer) *)serverFromJSONDictionary : (NSDictionary *)dictionary;
+
+@end
